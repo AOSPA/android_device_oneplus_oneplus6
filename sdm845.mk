b0b0f5ef54d5246c05b532db12bd8a13fd22d6ef
LuK1337 <priv.luk@gmail.com>
2018-07-09 21:25:14 +0200

    sdm845-common: Add A/B updates support
    Change-Id: Id91fde75da524f1a9729ecd5e29d3fd741e7e11e

--- sdm845.mk
+++ sdm845.mk
@@ -24,14 +24,56 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Properties
 -include $(LOCAL_PATH)/common-props.mk
 
+# A/B
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    system \
+    vbmeta
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script
+
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
+
+# Boot control
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
 
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
+
+# Update engine
+PRODUCT_PACKAGES += \
+    brillo_update_payload \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service
+
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.sdm845 \
+    libcutils \
+    libgptutils \
+    libz \
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
 
 # VNDK-SP
 PRODUCT_PACKAGES += \
