0834af4263634d96e7fd011223f64895d7796011
TheStrix <parthbhatia98@gmail.com>
2019-06-17 19:51:28 +0530

    sdm845-common: Add system props
    Change-Id: I287bc6021eb21d3eafb659e3b42681007f80bf61

---
+++ common-props.mk
@@ -0,0 +1,137 @@
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    audio.deep_buffer.media=true \
+    audio.offload.buffer.size.kb=32 \
+    audio.offload.min.duration.secs=30 \
+    audio.offload.video=true \
+    av.offload.enable=true \
+    persist.audio.fluence.speaker=true \
+    persist.audio.fluence.voicecall=true \
+    persist.audio.fluence.voicerec=false \
+    persist.vendor.audio.hac.enable=false \
+    persist.vendor.audio_hal.dsp_bit_width_enforce_mode=24 \
+    ro.af.client_heap_size_kbyte=7168 \
+    ro.qc.sdk.audio.fluencetype=none \
+    ro.qc.sdk.audio.ssr=false \
+    use.voice.path.for.pcm.voip=true
+
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    bt.max.hfpclient.connections=1 \
+    persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
+    persist.vendor.btstack.enable.splita2dp=true \
+    ro.bluetooth.library_name=libbluetooth_qti.so \
+    vendor.bluetooth.soc=cherokee
+
+# CNE / DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.cne.feature=1 \
+    persist.vendor.dpm.feature=9
+
+# Coresight
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.debug.coresight.config=stm-events
+
+# Data modules
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.df.dev_name=rmnet_usb0 \
+    persist.data.iwlan.enable=true \
+    persist.vendor.data.mode=concurrent \
+    persist.vendor.data.iwlan.enable=true \
+    ro.vendor.use_data_netmgrd=true
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.enable_hwc_vds=1 \
+    persist.sys.force_sw_gles=1 \
+    ro.kernel.qemu.gles=0 \
+    persist.vendor.color.matrix=2
+
+# HWUI
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.layer_cache_size=48 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.text_large_cache_height=4096 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.texture_cache_size=72
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.aac_51_output_enabled=true \
+    media.settings.xml=/vendor/etc/media_profiles_vendor.xml \
+    media.stagefright.audio.deep=true \
+    media.stagefright.enable-aac=true \
+    media.stagefright.enable-fma2dp=true \
+    media.stagefright.enable-http=true \
+    media.stagefright.enable-player=true \
+    media.stagefright.enable-qcp=true \
+    media.stagefright.enable-scan=true \
+    mm.enable.smoothstreaming=true \
+    mmp.enable.3g2=true \
+    persist.mm.enable.prefetch=true
+
+#Netflix custom property
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.netflix.bsp_rev=Q845-14477-1
+
+# Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.core_ctl_max_cpu=4 \
+    ro.vendor.qti.core_ctl_min_cpu=2 \
+    ro.vendor.qti.sys.fw.bservice_age=5000 \
+    ro.vendor.qti.sys.fw.bservice_enable=true \
+    ro.vendor.qti.sys.fw.bservice_limit=5 \
+    vendor.iop.enable_prefetch_ofr=0 \
+    vendor.iop.enable_uxe=0
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    DEVICE_PROVISIONED=1 \
+    persist.radio.efssync=true \
+    persist.rcs.supported=0 \
+    persist.rild.nitz_long_ons_0="" \
+    persist.rild.nitz_long_ons_1="" \
+    persist.rild.nitz_long_ons_2="" \
+    persist.rild.nitz_long_ons_3="" \
+    persist.rild.nitz_plmn="" \
+    persist.rild.nitz_short_ons_0="" \
+    persist.rild.nitz_short_ons_1="" \
+    persist.rild.nitz_short_ons_2="" \
+    persist.rild.nitz_short_ons_3="" \
+    persist.vendor.ims.dropset_feature=0 \
+    persist.vendor.qmi_log_on=1 \
+    persist.vendor.radio.arfcn_test_mode=3 \
+    persist.vendor.radio.bar_fake_gcell=1 \
+    persist.vendor.radio.data_con_rprt=1 \
+    persist.vendor.radio.data_ltd_sys_ind=1 \
+    persist.vendor.radio.force_on_dc=true \
+    persist.vendor.radio.ignore_dom_time=10 \
+    ril.subscription.types=NV,RUIM \
+    ro.telephony.default_network=22,22 \
+    telephony.lteOnCdmaDevice=1,1
+
+# Time
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.timed.enable=true
+
+# Touchscreen
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.qti.inputopts.enable=true \
+    persist.vendor.qti.inputopts.movetouchslop=0.6
+
+# WiFi / WFD
+PRODUCT_PROPERTY_OVERRIDES += \
+    config.disable_rtt=true \
+    persist.debug.wfd.enable=1 \
+    persist.sys.wfd.virtual=0 \
+
+# Wipower
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bluetooth.emb_wp_mode=false \
+    ro.bluetooth.wipower=false
