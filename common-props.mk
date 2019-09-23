5fc17beeb4a129f3ea4550a56ee9ed22b82826f8
Jake Weinstein <jake@aospa.co>
2019-09-23 02:13:01 +0200

    sdm845-common: remove deprecated hwui and input properties
    These are unused in P and Q.
    
    Change-Id: Ifb1dc9b419a05cee156d1a6db4c0489834765ae5

--- common-props.mk
+++ common-props.mk
@@ -47,20 +47,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.kernel.qemu.gles=0 \
     persist.vendor.color.matrix=2
 
-# HWUI
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.layer_cache_size=48 \
-    ro.hwui.path_cache_size=32 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.text_large_cache_height=4096 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.texture_cache_size=72
-
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     media.aac_51_output_enabled=true \
@@ -118,11 +104,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Time
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
-
-# Touchscreen
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.qti.inputopts.enable=true \
-    persist.vendor.qti.inputopts.movetouchslop=0.6
 
 # WiFi / WFD
 PRODUCT_PROPERTY_OVERRIDES += \
