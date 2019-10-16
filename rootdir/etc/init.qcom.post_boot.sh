839abbbd225c0ebd48065f31dbc82674b73d26fe
Jake Weinstein <jake@aospa.co>
2019-10-16 07:23:30 +0200

    oneplus6: configure optimal cpusets
    These provide the best balance of performance and power on SDM845
    
    Change-Id: I49261089163847fd0ff929475de8ad2a31e6c90a

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -4222,8 +4222,9 @@ case "$target" in
         echo 10 > /sys/class/devfreq/soc:qcom,mincpubw/polling_interval
 
         # cpuset parameters
-        echo 0-3 > /dev/cpuset/background/cpus
-        echo 0-3 > /dev/cpuset/system-background/cpus
+        echo 0-1 > /dev/cpuset/background/cpus
+        echo 0-2 > /dev/cpuset/system-background/cpus
+        echo 0-3 > /dev/cpuset/restricted/cpus
 
         # Turn off scheduler boost at the end
         echo 0 > /proc/sys/kernel/sched_boost
