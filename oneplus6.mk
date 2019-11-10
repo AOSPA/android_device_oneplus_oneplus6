a6943188c1325c146ef583bd7fd9848bb8306601
Jake Weinstein <jake@aospa.co>
2019-11-10 03:53:33 +0100

    oneplus6: add QDCM profiles from OnePlus6-user-10-QKQ1.190716.003-1910050400-release-keys
    Stable display configuration is really dull.  Make AMOLED great again.
    
    Change-Id: Ib4206f108277f3e9d4fd75898b06a0e38199aa7a

--- oneplus6.mk
+++ oneplus6.mk
@@ -78,6 +78,11 @@ PRODUCT_PACKAGES += \
     libdisplayconfig \
     libqdMetaData.system
 
+# Display Calibration
+PRODUCT_PACKAGES += \
+    qdcm_calib_data_samsung_s6e3fc2x01_cmd_mode_dsi_panel.xml \
+    qdcm_calib_data_samsung_sofef00_m_cmd_mode_dsi_panel.xml
+
 # Fingerprint
 PRODUCT_COPY_FILES += \
     vendor/pa/config/permissions/vendor.pa.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.pa.biometrics.fingerprint.inscreen.xml
