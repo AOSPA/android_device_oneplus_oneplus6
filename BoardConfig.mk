7c0f3eaabca79f784352778668244380e55d990b
Jake Weinstein <jake@aospa.co>
2019-11-11 16:09:51 +0100

    Revert "Revert "oneplus6: add init script to set properties per variant""
    We are bringing this back because OnePlus6 was still showing for 6T users
    in some places.
    
    This reverts commit dd1f128c954f16a1f9187e184eabaddefdd6e262.
    
    Change-Id: Ic868e62dc04fbff39c3922ead88426cbbe5efd17

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,5 +167,9 @@ DISABLE_EAP_PROXY := true
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
 
+# Vendor init
+TARGET_INIT_VENDOR_LIB := libinit_oneplus6
+TARGET_RECOVERY_DEVICE_MODULES := libinit_oneplus6
+
 # Inherit from the proprietary version
 -include vendor/oneplus/sdm845-common/BoardConfigVendor.mk
