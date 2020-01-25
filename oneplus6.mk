9d56c82614110ea56ac46859aff83da51a8f5b4c
Jake Weinstein <jake@aospa.co>
2020-01-25 03:40:08 +0100

    Revert "oneplus6: add QDCM profiles from OnePlus6-user-10-QKQ1.190716.003-1910050400-release-keys"
    OnePlus fixed their DTS commands so this is no longer needed.
    
    This reverts commit a6943188c1325c146ef583bd7fd9848bb8306601.
    
    Change-Id: I3061ae4bc7c932536e2124ab0fd4c58de65eb74a

--- oneplus6.mk
+++ oneplus6.mk
@@ -82,11 +82,6 @@ PRODUCT_PACKAGES += \
     libqdMetaData \
     libqdMetaData.system
 
-# Display Calibration
-PRODUCT_PACKAGES += \
-    qdcm_calib_data_samsung_s6e3fc2x01_cmd_mode_dsi_panel.xml \
-    qdcm_calib_data_samsung_sofef00_m_cmd_mode_dsi_panel.xml
-
 # Fingerprint
 PRODUCT_COPY_FILES += \
     vendor/pa/config/permissions/vendor.pa.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.pa.biometrics.fingerprint.inscreen.xml
