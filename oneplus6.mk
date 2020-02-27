3fa043265ce9d2d517c58f7ee401d7327f6ae9d9
Jake Weinstein <jake@aospa.co>
2020-02-27 16:40:35 +0100

    oneplus6: Copy WiFi configuration to vendor_overlay
    This is unmodified from stock but will be changed in the next commits.
    
    Change-Id: If4c8dc2c5b8490dd3c7bad54650fc082500b2dec

--- oneplus6.mk
+++ oneplus6.mk
@@ -193,6 +193,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# Wi-Fi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR_OVERLAY)/etc/wifi/WCNSS_qcom_cfg.ini \
+
 # Common RRO Overlays
 PRODUCT_PACKAGES += \
     SDM845CommonBluetoothRes \
