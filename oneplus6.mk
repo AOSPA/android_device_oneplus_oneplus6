68218c0749c07f2e992e93f50e9ed77bba2d5e99
Jake Weinstein <jake@aospa.co>
2020-04-01 02:53:12 +0200

    oneplus6: move radio packages to device/qcom/common/telephony
    Change-Id: I10308ec9211db4ee75b422cd799b0fc829658e1c

--- oneplus6.mk
+++ oneplus6.mk
@@ -145,16 +145,6 @@ TARGET_COMMON_QTI_COMPONENTS := \
     telephony \
     wfd
 
-# Radio
-PRODUCT_PACKAGES += \
-    android.hardware.radio@1.0 \
-    android.hardware.radio@1.1 \
-    android.hardware.radio@1.2 \
-    android.hardware.radio@1.3 \
-    android.hardware.radio@1.4 \
-    android.hardware.radio.config@1.0 \
-    android.hardware.radio.deprecated@1.0
-
 # tri-state-key
 PRODUCT_PACKAGES += \
     TriStateHandler \
