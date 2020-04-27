c24901f19a4d1cabbd31745874eb6b3f5c08cbe4
Jake Weinstein <jake@aospa.co>
2020-04-27 16:49:06 +0200

    oneplus6: remove manual calls to display-commonsys-intf modules
    We'll do this in common
    
    Change-Id: I341f58826cf8e49e445b5119ba99ca77bb432bc0

--- oneplus6.mk
+++ oneplus6.mk
@@ -77,12 +77,6 @@ PRODUCT_PACKAGES += \
     init.qcom.post_boot.sh \
     ueventd.qcom.rc
 
-# Display
-PRODUCT_PACKAGES += \
-    libdisplayconfig \
-    libqdMetaData \
-    libqdMetaData.system
-
 # Display Calibration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qdcm_calib_data_samsung_s6e3fc2x01_cmd_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR_OVERLAY)/etc/qdcm_calib_data_samsung_s6e3fc2x01_cmd_mode_dsi_panel.xml \
